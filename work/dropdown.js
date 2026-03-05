a4fcc0a5aa0d065c1b7dab208b1b97e8e6a51611
SeliliPona <aidenborrell+github@gmail.com>
2026-03-05 11:26:26 -0700

    brand new page NOWAYING

---
+++ work/dropdown.js
@@ -0,0 +1,43 @@
+var previousDd = null
+var previousDdBtn = null
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function toggleDropdown(dropdown) {
+    var dd = document.getElementById(dropdown);
+    var ddbtn = document.getElementById("_"+dropdown)
+    if (dropdown == previousDd) {
+        dd.className = "dropdown-content";
+        ddbtn.className = "dropbtn"
+        previousDd = null;
+    } else {
+        if (previousDd != null) {
+            document.getElementById(previousDd).className = "dropdown-content";
+        }
+        dd.className += " show";
+        ddbtn.className += " open"
+        previousDd = dropdown;
+    }
+}
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+function showModal(image) {
+  modal.className += " show";
+  modalImg.src = "../assets/img/drawings/" + image + ".png";
+  captionText.innerHTML = image;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.className = "modal";
+  modal.style.display = "none";
+} 
